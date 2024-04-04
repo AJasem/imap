6ee79828be972a31105d6dbcb39e9356d3d1f4f0
AJasem <ajassem.ahmad@gmail.com>
2024-04-04 15:25:23 +0200

    nav

--- ipam/src/components/MainNav.jsx
+++ ipam/src/components/MainNav.jsx
@@ -26,14 +26,6 @@ const MainNav = () => {
             <Nav className="me-auto">
               <Nav.Link as={Link} to="/sign-in" onClick={handleNavLinkClick} active={location.pathname === '/sign-in'}>Login</Nav.Link>
               <Nav.Link as={Link} to="/sign-up" onClick={handleNavLinkClick} active={location.pathname === '/sign-up'}>Sign up</Nav.Link>
-              
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown" hidden>
-                <NavDropdown.Item href="#">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#">Another action</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#">Something else</NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="#" disabled hidden>Disabled</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
