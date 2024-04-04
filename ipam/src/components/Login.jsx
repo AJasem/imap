f3f9e0ddc7ad76e6a2ffe5e19f3a0d487baf4178
AJasem <ajassem.ahmad@gmail.com>
2024-04-04 23:52:50 +0200

    signinhook

--- ipam/src/components/Login.jsx
+++ ipam/src/components/Login.jsx
@@ -1,62 +1,36 @@
-import { useNavigate } from "react-router-dom";
-import React, { useState } from "react";
-import axios from "axios";
-import { Form, Input, Button, message } from "antd";
+import { Form, Input, Button } from "antd";
+import useLogin from "../hooks/useLogin";
 
 function Login() {
-  const navigate = useNavigate();
-  const [loading, setLoading] = useState(false);
+  
+  const { loading, login } = useLogin();
 
   const handleSubmit = async (values) => {
-    setLoading(true);
-    const { email, password } = values;
-    try {
-      const response = await axios.post("http://localhost:3005/login", {
-        email: email,
-        password: password,
-      });
-      const data = response.data;
-      if (response.status === 200) {
-        localStorage.clear();
-        localStorage.setItem("token", JSON.stringify(data.token));
-        navigate("/mailbox");
-      } else {
-        message.error(`${data.message}`);
-      }
-    } catch (error) {
-      if (error.response) {
-        const errorMessage = error.response.data.error;
-        message.error(`${errorMessage}`);
-      } else {
-        message.error(`${error.message}`);
-      }
-    } finally {
-      setLoading(false);
-    }
+    login(values);
   };
 
   return (
     <div className="form">
-    <Form onFinish={handleSubmit}>
-      <h3>Sign In</h3>
-      <Form.Item
-        name="email"
-        rules={[{ required: true, message: "Please input your email!" }]}
-      >
-        <Input placeholder="Enter email" />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[{ required: true, message: "@ahmads.dev" }]}
-      >
-        <Input.Password placeholder="Enter password" />
-      </Form.Item>
-      <Form.Item>
-        <Button type="primary" htmlType="submit" loading={loading}>
-          Sign In
-        </Button>
-      </Form.Item>
-    </Form>
+      <Form onFinish={handleSubmit}>
+        <h3>Sign In</h3>
+        <Form.Item
+          name="email"
+          rules={[{ required: true, message: "Please input your email!" }]}
+        >
+          <Input placeholder="Enter email" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: "@ahmads.dev" }]}
+        >
+          <Input.Password placeholder="Enter password" />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" loading={loading}>
+            Sign In
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
---
+++ ipam/src/hooks/useLogin.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { message } from "antd";
+
+const useLogin = () => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+
+  const login = async (values) => {
+    const { email, password } = values;
+    setLoading(true);
+
+    try {
+      const response = await axios.post("http://localhost:3005/login", {
+        email: email,
+        password: password,
+      });
+      const data = response.data;
+      if (response.status === 200) {
+        localStorage.clear();
+        localStorage.setItem("token", JSON.stringify(data.token));
+        navigate("/mailbox");
+      } else {
+        message.error(`${data.message}`);
+      }
+    } catch (error) {
+      if (error.response) {
+        const errorMessage = error.response.data.error;
+        message.error(`${errorMessage}`);
+      } else {
+        message.error(`${error.message}`);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { loading, login };
+};
+
+export default useLogin;
