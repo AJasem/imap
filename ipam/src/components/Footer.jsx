a3de39e8ef884f633cf5677e5aa730c6f2f92634
AJasem <ajassem.ahmad@gmail.com>
2024-06-22 18:54:12 +0200

    .

--- ipam/src/components/Footer.jsx
+++ ipam/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer className="footer">
             <p className='footer-item'>© 2024 Dmail</p>
             <a href='/about' className='footer-item' target='_blank' rel='noopener noreferrer'>about</a>
-            <p className='footer-item'>contact</p>
+            <p className='footer-item'>Contact</p>
             <a href='/API' className='footer-item' target='_blank' rel='noopener noreferrer'>API</a>
         </footer>
     );
